825d8400a6d1b3992c7cc6d3915f18ed851b74d3
milosz <git@miloszmilewski.dev>
2021-07-05 12:58:50 +0100

    active route styling

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 import "./navbar.css"
 
 function Navbar(){
     return (
         <nav className="navbar pt-4 space-x-2">
-            <Link className="navbutton" to="/">servers</Link>
+            <Link className="navbutton" to="/" exact>servers</Link>
             <Link className="navbutton" to="/games">games</Link>
             <Link className="navbutton" to="/orgs">organizations</Link>
         </nav>
