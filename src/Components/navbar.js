134a71e1103364a90fa12b6959917c467a50b4ca
milosz <git@miloszmilewski.dev>
2021-07-05 19:09:53 +0100

    yeet orgs out

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -6,7 +6,6 @@ function Navbar(){
         <nav className="navbar pt-4 space-x-2">
             <Link className="navbutton" to="/" exact>servers</Link>
             <Link className="navbutton" to="/games">games</Link>
-            <Link className="navbutton" to="/orgs">organizations</Link>
         </nav>
     )
 }
