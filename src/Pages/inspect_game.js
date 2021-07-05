676eb3f47d2fc61fd7baa6651c0eff63fab91b9d
milosz <git@miloszmilewski.dev>
2021-07-05 19:10:08 +0100

    badge changes and translation change

--- src/Pages/inspect_game.js
+++ src/Pages/inspect_game.js
@@ -33,19 +33,21 @@ function InspectGame(){
     return (
         <div className="max-w-4xl bg-gray-800 rounded-3xl p-4">
             <div className="flex">
-                <div className="w-32 relative place-self-center mr-4 mb-4" style={{aspectRatio: 1/1}}>
+                <div className="min-w-32 max-w-32 relative place-self-center mr-4 mb-4" style={{aspectRatio: 1/1}}>
                     <img src={asset.thumb} className="w-32 rounded-xl"></img>
                     <img src={asset.org.thumb} className="w-8 absolute rounded-xl bottom-1 right-1"></img>
                 </div>
 
-                <span className="inline-block w-100 max-w-full align-middle">
+                <span className="inline-blockalign-middle">
                     <p className="text-3xl">Game information about {asset.title}</p>
                     <p className="text-xl mb-1">A game made by {asset.org.title}</p>
-                    <p className="badge">Updated {howLongAgo}</p>
-                    {
-                        asset.download?.type ? <a className="badge" href={asset.download.url}>Project Link</a> : null
-                    }
-                    
+
+                    <div className="self-stretch">
+                        <p className="badge">Updated {howLongAgo}</p>
+                        {
+                            asset.download?.type ? <a className="badge" href={asset.download.url}>Project Link</a> : null
+                        }
+                    </div>
                 </span>
             </div>
 
