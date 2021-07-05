a58dd637224ce89a7cd24431dd989391d7468b16
milosz <git@miloszmilewski.dev>
2021-07-05 12:16:52 +0100

    BrowserRouter as router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/navbar';
 
